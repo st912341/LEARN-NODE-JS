becad35515fd251610a5ef2c6ff64240171964a4
st912341 <71476272+st912341@users.noreply.github.com>
2023-07-30 13:22:01 +0530

    withCons

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-var productOfTwoNumber=(a,b)=>{
- return a*b;
+class student
+{
+    constructor(name, roll, age , marks)
+    {
+        this.roll=roll;
+        this.name=name;
+        this.age=age;
+        this.marks=marks;
+    }
+    display=()=>
+    {
+        console.log(this.name+" "+ this.roll);
+    }
 
 }
-console.log(productOfTwoNumber(5,7));
+
+obj1=new student('sourabh',19);
+obj1.display();
+
+
