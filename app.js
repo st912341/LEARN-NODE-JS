b9de691b079136776a3197d09207f6e6b4923193
st912341 <71476272+st912341@users.noreply.github.com>
2023-07-30 17:06:06 +0530

    createFirstServer

---
+++ app.js
@@ -0,0 +1,13 @@
+const http=require('http');
+const server=http.createServer((req, res)=>{
+    res.setHeader('Content-Type', 'text/plain');
+    //res.write('sourabh tiwari')
+    res.end();
+});
+const port=4000
+server.listen(port, ()=>{
+
+console.log(`the server is listning at  ${port}`)
+console.log('my name is sourabh tiwari');
+}
+);
