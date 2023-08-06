8aaf8ddef11a717c37adacf9c7ae9afe7f9bdeed
st912341 <71476272+st912341@users.noreply.github.com>
2023-08-06 11:19:26 +0530

    require_url

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
-var productOfTwoNumber=(a,b)=>{
- return a*b;
-
+function a()
+{
+    setTimeout(() => {
+        console.log("a");
+    }, 3000);
 }
-console.log(productOfTwoNumber(5,7));
+function b()
+{
+    setTimeout(() => {
+        console.log("b");
+    }, 2500);
+}
+function c()
+{
+    setTimeout(() => {
+        console.log("c");
+    }, 2000);
+}
+function d()
+{
+    setTimeout(() => {
+        console.log("d");
+    }, 1500);
+}
+function e()
+{
+    setTimeout(() => {
+        console.log("e");
+    }, 1000);
+}
+async function  fun()
+{
+await a();
+ await b();
+await c();
+await d();
+await e();
+}
+fun();
